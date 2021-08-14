dedd525cc11bcabf21c6d5a5d9e6c634e5323ac7
RedGuy <I.maisuradze31.19@gmail.com>
2021-08-14 15:29:42 +0400

    logic

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 console.log(btnsOpenModal);
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", function () {
+    console.log("Clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
